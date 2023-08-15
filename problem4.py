f5d0714825ea081a93f81f9559b8ae1de2b199a8
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-15 20:10:48 +0100

    Solution with for loops

--- problem4.py
+++ problem4.py
@@ -13,13 +13,31 @@ import math
 
 
 def solution(digits):
-    # Work out range from 10 to 99
-    all_relevant_numbers = range(10 ** (digits - 1), 10 ** digits)
+    lowest_relevant_number = 10 ** (digits - 1)
+    highest_relevant_number = 10 ** digits
+    all_relevant_numbers = range(lowest_relevant_number , highest_relevant_number)
     all_relevant_products = [x * y for y in all_relevant_numbers for x in all_relevant_numbers]
     all_palindromes = filter(lambda x: str(x)[::-1] == str(x), all_relevant_products)
     return max(all_palindromes)
 
 
+def solution_with_for_loops(digits):
+    lowest_relevant_number = 10 ** (digits - 1)
+    highest_relevant_number = 10 ** digits
+    all_relevant_numbers = range(lowest_relevant_number , highest_relevant_number)
+    candidate = 0
+    for x in all_relevant_numbers:
+        for y in all_relevant_numbers:
+            product = x * y
+            if str(product)[::-1] != str(product):
+                continue
+            if product > candidate:
+                candidate = product
+    return candidate
+
+
 if __name__ == '__main__':
     answer = solution(3)
+    answer_with_for_loops = solution_with_for_loops(3)
     print(answer)
+    print(answer_with_for_loops)
